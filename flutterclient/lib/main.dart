0b2548816819b8ebdbcaffcce26ffab241b9c6e5
hongtu_zang <zanghongtu2006@gmail.com>
2024-01-18 15:05:20 +0800

    generated files

---
+++ flutterclient/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'page/pages/home_page.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      home: HomePage(),
+    );
+  }
+}
