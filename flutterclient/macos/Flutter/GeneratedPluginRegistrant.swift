4cd9b0e2cdec5babf49c2672fdb4d327266d89de
hongtu_zang <zanghongtu2006@gmail.com>
2024-03-12 15:00:10 +0800

    support upload file

--- flutterclient/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutterclient/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import file_selector_macos
 import flutter_secure_storage_macos
+import image_editor_common
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  ImageEditorPlugin.register(with: registry.registrar(forPlugin: "ImageEditorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
